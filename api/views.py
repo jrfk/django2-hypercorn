fcc20cd8de8ff4176aac203350cd6e7eb7e6e0b5
yujichino <chino@jsl.co.jp>
2020-10-10 23:03:30 +0900

    add .gitignore

--- api/views.py
+++ api/views.py
@@ -1,8 +1,9 @@
 from django.http.response import JsonResponse
 from django.views import View
 
+
 class HelloQueryView(View):
+
     def get(self, request, *args, **kwargs):
         ids = request.GET.get('id', '')
         return JsonResponse({"text": f"Hello python, {ids}!"})
-
